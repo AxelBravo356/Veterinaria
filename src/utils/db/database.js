dd2810cfd65a56b97d51ddba40f0b5af1897d59a
jvmojuan228 <jvmo.morales5a@hotmail.com>
2023-06-15 20:14:14 -0600

    intento 2 bd

--- src/utils/db/database.js
+++ src/utils/db/database.js
@@ -9,9 +9,9 @@ import { createPool } from "mysql2/promise";
 // });
 const pool = createPool({
     "database": "veterinaria",
-    "user": "xhuv6nxfsmze2p1sbwdc",
-    "host": "aws.connect.psdb.cloud",
-    "password": "pscale_pw_EjAf92PovO4YtBMwDnQ23cDdEMv8by0BNIji8bbdSTt",
+  "user": "hya8s9jvx7tju3liqbme",
+  "host": "aws.connect.psdb.cloud",
+  "password": "pscale_pw_1nkmS748DdvaUP1POBSGiRQNEmQoa6IXrOITSZDXnrA",
     "ssl": {
         rejectUnauthorized: true
     }
